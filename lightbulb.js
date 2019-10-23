e9e64844d3a0dac7464055f427e8dae8dc1bf3e5
lokeNsvelland <lokesvelland@Lokes-MacBook-Air.local>
2019-10-23 10:54:01 +0200

    lightbulb

---
+++ lightbulb.js
@@ -0,0 +1,17 @@
+function light(sw) {
+    var pic;
+    if (sw == 0) {
+        pic = "pic_bulboff.gif"
+        document.getElementById('everything').classList.add("hidden");
+        document.body.style.backgroundColor = "black";
+    } else {
+        pic = "pic_bulbon.gif";
+        document.getElementById('everything').classList.remove("hidden");
+        document.body.style.backgroundColor = "gray";
+        //document.getElementById('skjermbilde').src = pic;
+    }
+    document.getElementById('myImage').src = pic;
+   
+}
+
+//document.getElementById('skjermbilde').src = pic;

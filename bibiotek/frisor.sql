58a5acefd1db453463b8706a4ba071d2634916a4
Loke Svelland <lokesvelland@gmail.com>
2019-11-19 14:41:41 +0100

    kleg

--- bibiotek/frisor.sql
+++ bibiotek/frisor.sql
@@ -1,19 +1,8 @@
-create table klipp(
-klippid serial primary key,
-klipptime date not null,
-betalt text DEFAULT 'nei' check (
-    betalt = 'ja'
-    or ikke-betalt = 'nei'
-),
-pris int
-);
-
 create table frisor(
 frisorid serial primary key,
 fornavn text not null,
 etternavn text not null,
-mobil text,
-klippid int REFERENCES klipp (klippid)
+mobil text
 );
 
 create table kunde(
@@ -24,6 +13,25 @@ mobil text,
 kjonn text check (
     kjonn = 'f'
     or kjonn = 'm'
-),
-klippid int REFERENCES klipp (klippid)
+)
 );
+
+create table klipp(
+klippid serial primary key,
+klipptime date not null,
+betalt text DEFAULT 'nei' check (
+    betalt = 'ja'
+    or ikke-betalt = 'nei'
+),
+pris int DEFAULT = 1200,
+frisorid int REFERENCES frisor (frisorid),
+kundeid int REFERENCES kunde (kundeid)
+);
+
+insert into kunde (fornavn,etternavn,mobil) values ('ole','olsen','12345678'),('anne','olsen','87654321');
+
+delete from kunde where fornavn = 'ole';
+
+update klipp set pris = 200;
+
+select * from kunde where fornavn ~ 'kri';

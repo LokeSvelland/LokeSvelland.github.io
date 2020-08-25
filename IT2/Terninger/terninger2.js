edb5cc61016de8610bfe05855ad82027d2ee9905
Loke Svelland <lokesvelland@gmail.com>
2020-08-25 10:58:07 +0200

    terninger nr.2

--- IT2/Terninger/terninger2.js
+++ IT2/Terninger/terninger2.js
@@ -1,15 +1,32 @@
-//@ts-check
+// @ts-check
 
 function setup() {
-    let terning = 1;
-    let btnroll = document.getElementById("roll")
-    alert("hei")
+    let t1 = document.getElementById("t1"). src = randomnumber() + (".png");
+    let t2 = document.getElementById("t2");
+    let t3 = document.getElementById("t3");
+    let t4 = document.getElementById("t4");
+    let t5 = document.getElementById("t5");
 
-    btnroll.addEventListener("click", rollDice)
+    // let divKnapper = document.getElementById("knapper");
+    // let divMeldinger = document.getElementById("meldinger");
+    let btnRoll = document.getElementById("roll");
 
-    function rollDice() {
-        terning = Math.trunc(Math.random()) * 6;
-        console.log(terning);
-        divSpill.innerHTML = terning;
+    btnRoll.addEventListener("click", rullTerning);
+
+    function rullTerning() {
+        let terning = Math.trunc(Math.random() * 6 + 1);
+        t1.innerHTML = String(terning);
+
+        terning = Math.trunc(Math.random() * 6 + 1);
+        t2.innerHTML = String(terning);
+        
+        terning = Math.trunc(Math.random() * 6 + 1);
+        t3.innerHTML = String(terning);
+        
+        terning = Math.trunc(Math.random() * 6 + 1);
+        t4.innerHTML = String(terning);
+        
+        terning = Math.trunc(Math.random() * 6 + 1);
+        t5.innerHTML = String(terning);
     }
 }

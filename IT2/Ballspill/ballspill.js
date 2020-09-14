02830e270c6e4892c09bc9ecf25fc136a260e304
Loke Svelland <lokesvelland@gmail.com>
2020-09-14 08:03:30 +0200

    bubble + ballspill

--- IT2/Ballspill/ballspill.js
+++ IT2/Ballspill/ballspill.js
@@ -16,12 +16,12 @@
  * (vx,vy) er fart, w,h er width,height
  */
 class Ball {
-    x=0;
-    y=0;
-    w=0;
-    h=0;
-    vx=0;
-    vy=0;
+    x = 0;
+    y = 0;
+    w = 20;
+    h = 20;
+    vx = 0;
+    vy = 0;
     div = undefined;
 
     /**
@@ -30,7 +30,22 @@ class Ball {
     tegnPaaSkjerm() {
         this.div.style.left = this.x + "px";
         this.div.style.top = this.y + "px";
+        this.div.style.width = this.w + "px";
+        this.div.style.height = this.h + "px";
     }
+}
+
+/**
+ * ballTabell inneholder alle ballene i spillet
+ */
+const ballTabell = [];
+let antall = 100;
+
+function rndColor() {
+    const colors = "red,green,blue,yellow,pink,teal,aqua,orange".split(",");
+    const antall = colors.length;
+    const indeks = Math.trunc( Math.random() * antall);
+    return colors[indeks];
 }
 
 /**
@@ -40,24 +55,60 @@ class Ball {
 function setup() {
     const divBrett = document.getElementById("brett");
     const divStatus = document.getElementById("status");
-    const b = new Ball();
-    b.x = 100;
-    b.y = 100;
-    b.h = 20;
-    b.w = 20;
-    b.vx = 5;
-    b.vy = 0;
-    b.div = document.createElement("div");
-    divBrett.append(b.div);
-    b.div.className = "ball";
+    const inpAntall = /** @type {HTMLInputElement} */
+    (document.getElementById("antall"));
+    const btnStart = document.getElementById("start");
+    const maxY = Math.random() * 7 + 4;
+    const maxX = Math.random() * 7 + 4;
 
-    b.tegnPaaSkjerm();
+    btnStart.addEventListener("click", startSpillet);
+    function startSpillet() {
+        antall = Number(inpAntall.value);
+        for (let i = 0; i < antall; i += 1) {
+            const b = new Ball();
+            b.x = Math.random() * 460 + 20;
+            b.y = Math.random() * 460 + 20;
+            b.w = Math.random() * 100 + 2;
+            b.h = b.w;
+            b.vx = 5;
+            b.vy = -4;
+            b.div = document.createElement("div");
+            divBrett.append(b.div);
+            b.div.className = "ball";
+            b.div.style.backgroundColor = rndColor();
+            b.tegnPaaSkjerm();
+            ballTabell.push(b);
+        }
+        setInterval(flyttPaaBallen, 20);
+    }
 
-    setInterval(flyttPaaBallen,20);
+
+
 
     function flyttPaaBallen() {
-        b.x += 5;
-        b.tegnPaaSkjerm();
+        for (let i = 0; i < antall; i += 1) {
+            const b = ballTabell[i];
+            b.div.style.backgroundColor = rndColor();
+            b.x += b.vx;
+            b.y += b.vy;
+            if (b.x > 500 - b.w) {
+                b.vx = -maxX;
+                b.div.style.backgroundColor = rndColor();
+            }
+            if (b.x < 0) {
+                b.vx = maxX;
+                b.div.style.backgroundColor = rndColor();
+            }
+            if (b.y > 500 - b.h) {
+                b.vy = -maxY;
+                b.div.style.backgroundColor = rndColor();
+            }
+            if (b.y < 0) {
+                b.vy = maxY;
+                b.div.style.backgroundColor = rndColor();
+            }
+            b.tegnPaaSkjerm();
+        }
     }
 
 }

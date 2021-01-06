324a8a18fb45f2bdcb1bf1a14a4834b57665dc78
Loke Svelland <lokesvelland@gmail.com>
2021-01-06 09:38:21 +0100

    bhhj

---
+++ IT2/Julekort/julekort.js
@@ -0,0 +1,38 @@
+// @ts-check
+
+class Nisse {
+    x=-200;
+    y=100;
+    vx = 5;
+    div;
+
+    render() {
+        if (this.vx < 0) {
+            this.div.style.transform = "scaleX(-1)";
+        } else {
+            this.div.style.transform = "scaleX(1)";
+        }
+        this.div.style.left = `${this.x}px`;
+        this.div.style.top = `${this.y}px`;
+    }
+}
+
+
+function setup() {
+    const nisse = new Nisse();
+    nisse.div = document.getElementById("santa");
+
+    setInterval(() => {
+        nisse.x += nisse.vx;
+        nisse.render();
+
+        if (nisse.x > 1300) {
+            nisse.vx = -5;
+        }
+        if (nisse.x < 0) {
+            nisse.vx = 5;
+        }
+        
+    }, 10);
+
+}

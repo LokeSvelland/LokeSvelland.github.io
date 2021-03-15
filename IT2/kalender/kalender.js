a7e628420ce5ad3b0ff51a2b8e832f914dc1f4da
Loke <lokesvelland@gmail.com>
2021-03-15 09:56:02 +0100

    oiuj

--- IT2/kalender/kalender.js
+++ IT2/kalender/kalender.js
@@ -56,23 +56,63 @@ function Setup() {
     }
 }
 
-const divTest = document.getElementById("test");
-drawMonth(1990,5,divTest);
+/**
+ * Skal returnere 0 eller 1 avhengig om det
+ * er skuddår og m === 1 (februar)
+ * @param {number} y år
+ * @param {number} m måned
+ * @returns {number} 0|1
+ */
+function skudd(y, m) {
+    let s = 0;
+    // noe kode
+    return s;
+}
 
 /**
- * Skal tegne en måned gitt år, mnd og 
- * en div til å rendre i
- * @param {number} y gjeldende år
- * @param {number} m 0..11 måned-nr
- * @param {HTMLElement} div div hvor måed skal rendres
+ * Returnere antall dager for gitt y,m
+ * @param {number} y år
+ * @param {number} m måned
+ * @returns {number} antall dager i gitt måned
  */
-function drawMonth(y,m,div) {
+function dagerImaaned(y, m) {
+    const dager = [31, 28, 31, 30, 31,]
+    // skudd
+    return 31;
+}
+
+
+/**
+ * Skal tegne en måned gitt år,mnd og
+ * en div til å rendre i
+ * @param {number} y Gjeldende år
+ * @param {number} m 0..11 måned-nr
+ * @param {HTMLElement} div Div hvor måned skal rendres
+ */
+function drawMonth(y, m, div) {
     div.innerHTML = "";
-    let s = "";
     div.classList.add("month");
-    s +=`
+    let dagene = "";
+    for (let i = 1; i < 42; i++) {
+        dagene += `<span>${i}</span>`;
+    }
+    let s = "";
+    s += `
     <fieldset>
-        <legend> ${mNavn[m]} </legend>
+       <legend>${mNavn[m]} </legend>
+       <div class="ukedager">
+         <span>Ma</span> 
+         <span>Ti</span> 
+         <span>On</span> 
+         <span>To</span> 
+         <span>Fr</span> 
+         <span>Lø</span> 
+         <span>Sø</span>
+       </div>
+       <div class="dager">
+         ${dagene}
+       </div>
     </fieldset>`;
     div.innerHTML = s;
 }
+
